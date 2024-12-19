4a91852b1265be1342f807b77c17194bbd67a71a
Aphrodite <carterolsen2211@gmail.com>
2024-12-19 15:15:54 -0800

    Update __init__.py

--- Anubis/__init__.py
+++ Anubis/__init__.py
@@ -1,1 +1,11 @@
+__version__ = "1.0.0"
+__all__ = ["visualizations", "utils", "config"]
 
+# Import key functionalities for easy access
+from .visualizations import *
+from .utils import *
+from .config import *
+
+# Package metadata
+__author__ = "Anubis"
+__license__ = "Apache License 2.0"
